f47f9bab5a148193893adfa392f7029e638dbce3
Dalar-Coin <Dalarcoinmusic@gmail.com>
2024-12-15 01:56:25 -0500

    more backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const Product = require('./models/product.model.js')
+const Product = require('./models/product.model')
 
 const app = express()
 app.use(express.json())
@@ -9,11 +9,12 @@ app.get('/', (req, res) => {
   res.send('Hello from Node API Edit');
 });
 
-app.post('/api/products', (req, res) => {
+app.post('/api/products', async (req, res) => {
   try {
-    
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({message: error.message});
   }
 });
 
